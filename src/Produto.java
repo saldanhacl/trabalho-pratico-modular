f2f32f35e539bf4e39f320cdc39de1d59593a67a
saldanhacl <lucas.saldanha5@gmail.com>
2017-10-27 17:37:21 -0200

    Criação e encapsulamento de atributos

--- src/Produto.java
+++ src/Produto.java
@@ -1,2 +1,27 @@
 public class Produto {
+
+    private String nomeDoProduto;
+    private double precoUnitario;
+    private int quantidadeProduto;
+
+    public Produto(String nomeDoProduto, double precoUnitario) {
+        this.nomeDoProduto = nomeDoProduto;
+        this.precoUnitario = precoUnitario;
+    }
+
+    public String getNomeDoProduto() {
+        return nomeDoProduto;
+    }
+
+    public void setNomeDoProduto(String nomeDoProduto) {
+        this.nomeDoProduto = nomeDoProduto;
+    }
+
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
 }

677c41e989691bf12230df8c8ba91b6a33ea0f64
saldanhacl <lucas.saldanha5@gmail.com>
2017-10-28 20:14:05 -0200

    Criação da classe Aplicação para testar o programa

---
+++ src/Aplicacao.java
@@ -0,0 +1,20 @@
+public class Aplicacao {
+
+    public static void main(String[] args) {
+
+        Empresa pp = new Empresa("PiedPiper", "36.482.265/0001-10");
+
+        pp.cadastrarVendedor(new Vendedor("Pedro", "Henrique", "(31)97361-7174",
+                                        "29.870.405-5", "924.892.200-71", "Masculino"));
+        pp.cadastrarCliente(new Cliente("Rafael", "(31)97362-8463", 18, "063.756.140-66",
+                                        "Masculino", "Badaró"));
+        pp.cadastrarProduto(new Produto("Leite", 2.0, 100));
+
+        pp.mostrarClientes();
+        pp.mostrarProdutos();
+        pp.mostrarVendedores();
+
+
+    }
+
+}

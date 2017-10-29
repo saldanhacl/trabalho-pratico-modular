1445f8d33c474c2b8325f156bc7e3cf240fbb5f7
saldanhacl <lucas.saldanha5@gmail.com>
2017-10-29 15:20:37 -0200

    Novo teste de janela

---
+++ src/TesteJanelaCliente.java
@@ -0,0 +1,10 @@
+public class TesteJanelaCliente {
+
+
+    public static void main(String[] args) {
+        FrameCliente apg = new FrameCliente();
+        apg.setVisible(true);
+    }
+
+}
+
--- src/testeFrameCliente.java
+++
@@ -1,8 +0,0 @@
-
-public class teste {
-	public static void main(String[] args) {
-		FrameCliente apg = new FrameCliente();
-		apg.setVisible(true);
-	}
-
-}

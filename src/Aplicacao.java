6f0afe9caf93e52d06e2ed51c561bf463176c8e2
saldanhacl <lucas.saldanha5@gmail.com>
2017-10-30 16:00:52 -0200

    União das classes para teste

--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -6,16 +6,20 @@ public class Aplicacao {
 
         pp.cadastrarVendedor(new Vendedor("Pedro", "Henrique", "(31)97361-7174",
                                         "29.870.405-5", "924.892.200-71", "Masculino"));
+        pp.cadastrarVendedor(new Vendedor("Lucas", "Saldanha", "(31)97361-7174",
+                "29.870.405-5", "924.892.200-71", "Masculino"));
         pp.cadastrarCliente(new Cliente("Rafael", "(31)97362-8463", 18, "063.756.140-66",
                                         "Masculino", "Badaró"));
         pp.cadastrarProduto(new Produto("Leite", 2.0, 100));
 
-        pp.mostrarClientes();
-        pp.mostrarProdutos();
-        pp.mostrarVendedores();
+//        pp.mostrarClientes();
+//        pp.mostrarProdutos();
+//        pp.mostrarVendedores();
 
-        pp.agendarVisita();
-        pp.getVendedoresCadastrados().get(0).getAgenda().visitasAgendadas();
+        FrameCliente apg = new FrameCliente(pp);
+        apg.setVisible(true);
+
+
 
     }
 
